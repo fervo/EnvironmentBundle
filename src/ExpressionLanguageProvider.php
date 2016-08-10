17bf028647755a2c0f9880ed80420db7e783baf9
Magnus Nordlander <magnus@nordlander.se>
2016-08-10 17:45:56 +0200

    Updated to match PR for symfony/symfony.

--- src/ExpressionLanguageProvider.php
+++ src/ExpressionLanguageProvider.php
@@ -15,11 +15,36 @@ class ExpressionLanguageProvider implements ExpressionFunctionProviderInterface
     public function getFunctions()
     {
         return array(
-            new ExpressionFunction('env', function ($arg) {
-                return sprintf('getenv(%s)', $arg);
-            }, function (array $variables, $value) {
-                return getenv($value);
+            new ExpressionFunction('env', function ($arg, $default = null) {
+                if (2 > func_num_args()) {
+                    return sprintf('\\Fervo\\EnvironmentBundle\\ExpressionLanguageProvider::getEnvironmentVariable(%s)', $arg);
+                }
+
+                return sprintf('\\Fervo\\EnvironmentBundle\\ExpressionLanguageProvider::getEnvironmentVariable(%s, %s)', $arg, $default);
+            }, function (array $variables, $value, $default = null) {
+                if (3 > func_num_args()) {
+                    return ExpressionLanguageProvider::getEnvironmentVariable($value);
+                }
+
+                return ExpressionLanguageProvider::getEnvironmentVariable($value, $default);
             }),
         );
+    }
+
+    public static function getEnvironmentVariable($name, $default = null)
+    {
+        if (isset($_ENV[$name])) {
+            return $_ENV[$name];
+        }
+
+        if (false !== $value = getenv($name)) {
+            return $value;
+        }
+
+        if (2 > func_num_args()) {
+            throw new \RuntimeException("Environment variable $name not found.");
+        }
+
+        return $default;
     }
 }
